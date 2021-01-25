5654c4e9c66f25d1528051078cf6ce2ff859b451
larryw3i <larryw3i@163.com>
2021-01-25 12:30:50 +0800

    Just backup

---
+++ git_push.sh
@@ -0,0 +1,5 @@
+echo 'Push to git@gitee.com:larryw3i/Funing.git'
+git push git@gitee.com:larryw3i/Funing.git
+
+echo 'Push to git@github.com:larryw3i/Funing.git'
+git push git@github.com:larryw3i/Funing.git
