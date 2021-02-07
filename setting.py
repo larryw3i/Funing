2ce7d12d500a78fc0af1f72caea77b5b63df1bb4
larryw3i <larryw3i@163.com>
2021-02-07 22:07:20 +0800

    (^_^)

--- setting.py
+++ setting.py
@@ -6,12 +6,13 @@ import yaml
 usr_home = os.path.expanduser('~')
 base_dir = os.path.join( usr_home , '.funing')
 project_path = os.path.abspath( os.path.dirname(  __file__  ) )
-locale_path = os.path.join( base_dir, 'locale')
+locale_path = os.path.join( project_path, 'locale')
 
 setting_path = os.path.join( base_dir , 'setting.yml') 
 setting_example_path = os.path.join( project_path , 'setting.yml.example') 
 
 if not os.path.exists( setting_path ):
+    os.mkdir( base_dir )
     shutil.copyfile( setting_example_path, setting_path )
     
 data_dir = os.path.join( base_dir, 'data' )
