d0a41401d6a2181e622aff248c6f6232076b397d
larryw3i <larryw3i@163.com>
2022-06-25 23:14:25 +0800

    ~zZ

---
+++ funing/.lwcp/locale/__init__.py
@@ -0,0 +1,27 @@
+import gettext
+import locale
+import os
+import sys
+
+from funing.settings import *
+
+sys_lang_code = locale.getdefaultlocale()[0]
+locale_path = locale_dir_path = os.path.abspath(os.path.dirname(__file__))
+locale_langcodes = [
+    d
+    for d in os.listdir(locale_path)
+    if os.path.isdir(os.path.join(locale_path, d))
+]
+
+if sys_lang_code not in locale_langcodes:
+    sys_lang_code = "en_US"
+
+lang = gettext.translation(
+    app_name, localedir=locale_path, languages=[sys_lang_code]
+)
+
+lang.install()
+
+_ = lang.gettext
+
+t = T = _
