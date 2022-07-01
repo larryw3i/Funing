3431fd2b49fa197aff7db69cd05d7475743662d6
larryw3i <larryw3i@163.com>
2022-07-01 23:12:48 +0800

    backuo

--- funing/widgets/main.py
+++ funing/widgets/main.py
@@ -40,29 +40,59 @@ class MainWidget:
     def __init__(self, title=None):
         self.root = Tk()
         self._title = title or (_("Funing") + f" ({app_version})")
-        self._width = self._height = self.x = self.y = self.default_xywh= 10
-        self._geometry = (self._width, self._height, self._x, self._y)
-        self._defaut_geometry = self._geometry
+        self._width = self._height = self.x = self.y = self.default_xywh = 10
 
     def set_title(self, title=None):
         if not title:
             self._title = title
         self.root.title(self._title)
 
+    def set_x(self, x):
+        self._x = x
+
+    def set_y(self, y):
+        self._y = y
+
+    def set_height(self, height):
+        self._height = height
+
+    def set_width(self, width):
+        self._width = width
+
+    def get_x(self):
+        self._x = (
+            self.get_screenwidth(of=4)
+            if self._x == self.default_xywh
+            else self._x
+        )
+        return self._x
+
+    def get_y(self):
+        self._y = (
+            self.get_screenheight(of=4)
+            if self._y == self.default_xywh
+            else self._y
+        )
+        return self._y
+
     def get_geometry(self):
-        return self._geometry
-    
+        return (self._width, self._height, self._x, self._y)
+
     def get_width(self):
-        return self._width
+        self._width = (
+            self.get_screenheight(of=2)
+            if self._width == self.default_xywh
+            else self._y
+        )
+        return self._y
 
     def get_height(self):
+        self._height = (
+            self.get_screenheight(of=2)
+            if self._height == self.default_xywh
+            else self._height
+        )
         return self._height
-    
-    def get_x(self):
-        return self._x
-    
-    def get_y(self):
-        return self._y
 
     def get_geometry_str(self, geometry=None):
         geometry = geometry or self.get_geometry()
@@ -70,7 +100,7 @@ class MainWidget:
 
     def set_geometry(self, geometry=None, update_widget=True):
         if not geometry:
-            self._geometry = (
+            (
                 self._width,
                 self._height,
                 self.x,
@@ -79,11 +109,11 @@ class MainWidget:
         if update_widget:
             self.root.geometry(self.get_geometry_str())
 
-    def get_screenheight(self, of = 1):
-        return int( self.root.winfo_screenheight()/of)
+    def get_screenheight(self, of=1):
+        return int(self.root.winfo_screenheight() / of)
 
-    def get_screenwidth(self, of = 1):
-        return int(self.root.winfo_screenwidth()/of)
+    def get_screenwidth(self, of=1):
+        return int(self.root.winfo_screenwidth() / of)
 
     def get_cp(self, key):
         if self.cp == None:
