4fa8829275e6c39c1fbd3e0decc25e0dd47647e4
larryw3i <larryw3i@163.com>
2022-07-08 22:33:56 +0800

    ~zZ

--- funing/widgets/frame.py
+++ funing/widgets/frame.py
@@ -39,15 +39,53 @@ from funing.widgets.abc import *
 class FrameWidget(WidgetABC):
     def __init__(self, mw, text):
         super().__init__(mw)
+        self.parent = self.parent_widget = self.mw.left_widget
         self.text = text
+        self.label = None
+        self.frame_size = 25
+        self.video_src = None
+        self.image_src = None
+        self.openfrom_button = None
+        self.detectsrc_button = None
+
+    def start_video(self):
+        pass
+
+    def pause_video(self):
+        pass
+
+    def show_image(self):
+        pass
+
+    def get_frame(self):
+        pass
+
+    def get_video_src(self):
+        pass
+
+    def set_video_src(self):
+        pass
+
+    def get_image_src(self):
+        pass
+
+    def set_image_src(self):
+        pass
+
+    def set_frame_size(self, frame=25):
+        self.frame_size = frame
+
+    def get_frame_size(self):
+        return self.frame_size
 
     def get_max_width(self):
-        pass
+        return self.parent.get_text_width()
 
     def get_max_height(self):
-        pass
+        return int(self.parent.get_text_height() * 0.8)
 
     def set_widgets(self):
+        self.label = Label(self.root, text=_("Video frame label."))
         pass
 
     def set_x(self):
@@ -72,5 +110,4 @@ class FrameWidget(WidgetABC):
         pass
 
     def place(self):
-        pass
         pass
